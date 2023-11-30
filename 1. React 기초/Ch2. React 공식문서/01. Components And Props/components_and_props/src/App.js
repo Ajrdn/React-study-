2b84c2f6ec824828bf8cb40c12657155ecf1e2e4
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-11-30 21:12:13 +0900

    01. Components And Props

---
+++ 1. React 기초/Ch2. React 공식문서/01. Components And Props/components_and_props/src/App.js
@@ -0,0 +1,14 @@
+import Composition from './components/Composition'
+import Extraction from './components/Extraction/Extraction'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <Extraction />
+      <Composition />
+    </div>
+  )
+}
+
+export default App
