e46d9b5cc08511e06c576f9970a8dbc19946bb68
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-11-30 21:12:45 +0900

    02. State

---
+++ 1. React 기초/Ch2. React 공식문서/02. State/state/src/App.js
@@ -0,0 +1,14 @@
+import ClassComponent from './components/ClassComponent'
+import FunctionalComponent from './components/FunctionalComponent'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <FunctionalComponent />
+      <ClassComponent />
+    </div>
+  )
+}
+
+export default App
