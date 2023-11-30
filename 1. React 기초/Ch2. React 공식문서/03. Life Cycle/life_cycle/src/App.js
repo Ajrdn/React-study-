0136795620391df34407c998b6a09d839eaa279d
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-11-30 21:13:11 +0900

    03. Life Cycle

---
+++ 1. React 기초/Ch2. React 공식문서/03. Life Cycle/life_cycle/src/App.js
@@ -0,0 +1,12 @@
+import ClassComponent from './components/ClassComponent'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <ClassComponent />
+    </div>
+  )
+}
+
+export default App
