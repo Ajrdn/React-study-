813485ffa352c751d81b86e3fc90b34e8f1c2e1e
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-12-01 20:47:06 +0900

    08. Hooks

---
+++ 1. React 기초/Ch2. React 공식문서/08. Hooks/hooks/src/App.js
@@ -0,0 +1,14 @@
+import State from './components/State'
+import Reducer from './components/Reducer'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <State />
+      <Reducer />
+    </div>
+  )
+}
+
+export default App
