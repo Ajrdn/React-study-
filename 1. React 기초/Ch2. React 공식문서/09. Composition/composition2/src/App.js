dab148aaddb39f29f8b4935456097a57d17ce81f
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-12-01 20:47:32 +0900

    09. Composition

---
+++ 1. React 기초/Ch2. React 공식문서/09. Composition/composition2/src/App.js
@@ -0,0 +1,12 @@
+import ThankyouDialog from './components/ThankyouDialog'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <ThankyouDialog />
+    </div>
+  )
+}
+
+export default App
