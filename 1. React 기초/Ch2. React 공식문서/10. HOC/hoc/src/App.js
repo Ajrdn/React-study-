cd8535294d6bd62ed448be514b4fb71328c08d39
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-12-01 20:47:49 +0900

    10. HOC

---
+++ 1. React 기초/Ch2. React 공식문서/10. HOC/hoc/src/App.js
@@ -0,0 +1,16 @@
+import Input from './components/Input'
+import Button from './components/Button'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <Input />
+      <br />
+      <br />
+      <Button />
+    </div>
+  )
+}
+
+export default App
