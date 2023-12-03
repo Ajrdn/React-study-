5a9f869a7d91cd1768105f2da70b30639bbe853c
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-12-03 11:42:21 +0900

    DayJS

---
+++ 2. React 라이브러리/Ch1. 시간 라이브러리/DayJS/dayjs/src/App.js
@@ -0,0 +1,12 @@
+import DayjsExample from './components/DayjsExample'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <DayjsExample />
+    </div>
+  )
+}
+
+export default App
