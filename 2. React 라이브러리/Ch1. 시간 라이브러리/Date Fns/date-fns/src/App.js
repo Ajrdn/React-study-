1a02ef1a4bef143876b970dfb7dc25454f9bec0a
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-12-03 11:41:53 +0900

    Date Fns

---
+++ 2. React 라이브러리/Ch1. 시간 라이브러리/Date Fns/date-fns/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import DateFnsExample from './components/DateFnsExample'
+
+function App() {
+  return (
+    <div className='App'>
+      <DateFnsExample />
+    </div>
+  )
+}
+
+export default App
