23b1b3a05c7073b10796fc66bc38f92d313af764
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-12-03 11:44:02 +0900

    Emotion

---
+++ 2. React 라이브러리/Ch2. 스타일링 라이브러리/Emotion/emotion/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import EmotionExample from './components/EmotionExample'
+
+function App() {
+  return (
+    <div className='App'>
+      <EmotionExample />
+    </div>
+  )
+}
+
+export default App
